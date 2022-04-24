4650037f7a67958ccd267f34d0be0b2f58987991
sayeed83 <sayeedanari8383@gmail.com>
2022-04-24 23:55:19 +0530

    new chagne

--- app/controllers/reads3Data.js
+++ app/controllers/reads3Data.js
@@ -1,11 +1,12 @@
 var AWS = require('aws-sdk');
 exports.reads3Data = async (req, res) => {
+    console.log(' testing ')
     console.log(" req ", req.params.fileName);
     AWS.config.update({ 
-        accessKeyId: 'AKIA4BQMUAZRZ742TBLR',
-        secretAccessKey: '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',
-        region: 'ap-south-1',
-        signatureVersion: 'v4'
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY,
+      region: process.env.REGION,
+      signatureVersion: 'v4'
     });
 
     const s3 = new AWS.S3()
